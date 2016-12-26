0a5b10da1fa3bcce010582cd855e33620fd4c6a5
Jonathan Clem <jonathan@jclem.net>
2016-12-25 19:19:25 -0500

    Add Base62UUID.decode/1

--- lib/base62_uuid.ex
+++ lib/base62_uuid.ex
@@ -25,10 +25,35 @@ defmodule Base62UUID do
     |> ensure_length
   end
 
+  @doc """
+  Decodea an encoded UUID.
+  """
+  @spec decode(String.t) :: {:ok, String.t} | :error
+  def decode("0" <> uuid), do: decode(uuid)
+
+  def decode(uuid) do
+    with {:ok, int} <- Base62.decode(uuid),
+         raw_uuid <- Integer.to_string(int, 16) do
+      make_uuid(raw_uuid)
+    end
+  end
+
   @spec ensure_length(String.t) :: String.t
   defp ensure_length(encoded) when byte_size(encoded) == @length, do: encoded
   defp ensure_length(encoded) do
     difference = @length - String.length(encoded)
     String.duplicate("0", difference) <> encoded
+  end
+
+  @spec make_uuid(String.t) :: String.t
+  defp make_uuid(raw_uuid) do
+    <<gp_1::binary-size(8),
+      gp_2::binary-size(4),
+      gp_3::binary-size(4),
+      gp_4::binary-size(4),
+      gp_5::binary-size(12)>> = String.downcase(raw_uuid)
+
+    [gp_1, gp_2, gp_3, gp_4, gp_5]
+    |> Enum.join("-")
   end
 end
--- test/base62_uuid_test.exs
+++ test/base62_uuid_test.exs
@@ -8,4 +8,9 @@ defmodule Base62UUIDTest do
   test ".encode encodes a UUID" do
     assert Base62UUID.encode(UUID.uuid4) |> String.length == 22
   end
+
+  test ".decode decodes a UUID" do
+    uuid = UUID.uuid4
+    assert uuid |> Base62UUID.encode |> Base62UUID.decode == uuid
+  end
 end
